90fd5d5759de721a90116050119fe36e39a0762c
DenisTokar <45533926+DenisBlur@users.noreply.github.com>
2023-08-19 21:45:42 +0500

    Update enums.dart

--- lib/enums.dart
+++ lib/enums.dart
@@ -10,4 +10,26 @@ enum RadioFeedback {off, on, radioStarted, trackStarted, trackFinished, skip, ge
 
 enum RadioDiversity {favorite, discover, popular, defaultDiversity, none}
 enum RadioMoodEnergy {active, fun, calm, sad, all, none}
-enum RadioLanguage {russian, any, withoutWords, notRussian, none}
+enum RadioLanguage {russian, notRussian, withoutWords, any, none}
+
+List<String> diversityTranslate = [
+  "Любимое",
+  "Незнакомое",
+  "Популярное",
+  "Любое",
+];
+
+List<String> moodEnergyTranslate = [
+  "Бодрое",
+  "Весёлое",
+  "Спокойное",
+  "Грустное",
+  "Любое",
+];
+
+List<String> languageTranslate = [
+  "Русский",
+  "Иностранный",
+  "Без слов",
+  "Любой",
+];
